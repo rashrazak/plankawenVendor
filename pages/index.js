9c0fba92386c53787f322a5aaae259a773542f12
Rashdan Razak <rashdanrazak91@gmail.com>
2019-07-31 17:43:57 +0800

    plankawenVendor

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class index extends Component {
+    render() {
+        return (
+            <div>
+                <h1>This is vendor</h1>
+            </div>
+        )
+    }
+}
