3ebeb5f28fe4f1bb81cb756a0b27bea3e2fd1afa
Rashdan Razak <rashdanrazak91@gmail.com>
2019-08-05 17:58:19 +0800

    add bootstrap datatables and sweetalert

---
+++ pages/sweetTest.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import Swal from 'sweetalert2'
+import Navbar from 'react-bootstrap/Navbar'
+import NavItem from 'react-bootstrap/NavItem'
+
+
+const submitLoading =(e)=>{
+    Swal.showLoading();
+
+}
+
+class sweetTest extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hi Love</h1>
+                <button onClick={(e)=>submitLoading(e)}></button>
+            </div>
+        )
+    }
+}
+
+export default sweetTest
