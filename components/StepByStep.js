62b4aeea19cc748112dfc5e33657bdcc7307d05a
HaziqMohsin <myhaziq3@gmail.com>
2019-11-04 16:09:16 +0800

    [Update] Fix stepper

--- components/StepByStep.js
+++ components/StepByStep.js
@@ -3,8 +3,11 @@ import Stepper from 'react-stepper-horizontal'
 
 function StepByStep({progress}) {
     return (
-        <div>
+        <div className="stepper-div">
             <Stepper steps={ [{title: 'About'}, {title: 'Details'}, {title: 'Upload'},{title: 'Review'},{title: 'Done'}] } activeStep={ progress } />
+            <style jsx>{`
+                .stepper-div { max-width: 700px; margin: 30px auto; }
+            `}</style>
         </div>
     )
 }
