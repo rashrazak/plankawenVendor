531f58d948a4019853bffe816deb605698f6d65c
HaziqMohsin <myhaziq3@gmail.com>
2019-12-22 20:33:06 +0800

    [Update] Vendor signup ikut design

--- components/VendorForm.js
+++ components/VendorForm.js
@@ -27,6 +27,12 @@ function VendorForm() {
     const [facebook, setFacebook] = useState('');
     const [vendorId, setVendorId] = useState('')
 
+    const [firstDiv, setFirstDiv] = useState(true)
+    const [secondDiv, setSecondDiv] = useState(false)
+    const [thirdDiv, setThirdDiv] = useState(false)
+    const [forthDiv, setForthDiv] = useState(false)
+
+
     useEffect(() => {
         const y = async()=>{
             Swal.showLoading()
@@ -154,107 +160,173 @@ function VendorForm() {
         }
         
     }
- 
+
     return (
         <div className="">
             <div className="logo-container">
                 <img className="" src="/static/images/logos/logo-white-tagline.png"></img>
             </div>
             <div className="vendor-form-section">
-                <h3>VENDOR REGISTRATION FORM</h3>
+                <div className="">
+                    <img className="" src="/static/images/placeholder/vendor-bg.png"></img>
+                </div>
                 <div className="vendor-form">
-                    <div>
-                        <label>Email Syarikat</label>
-                        <Input className="form-custom" type="email" value={companyEmail} onChange={(e)=>setCompanyEmail(e.target.value)} required/>
-                        <label>Password</label>
-                        <Input className="form-custom" type="password" value={password} onChange={(e)=>setPassword(e.target.value)} required/>
-                        <label>Nama Syarikat</label>
-                        <Input className="form-custom" type="text" value={companyName} onChange={(e)=>setCompanyName(e.target.value)} required/>
-                        <label>Alamat Syarikat</label>
-                        <Input className="form-custom" type="textarea" value={companyAddress} onChange={(e)=>setCompanyAddress(e.target.value)} required/>
-                        <label>Nama Pemilik</label>
-                        <Input className="form-custom" type="text" value={owner} onChange={(e)=>setOwner(e.target.value)} required/>
-                        <label>Nombor Pendaftaran Syarikat</label>
-                        <Input className="form-custom" type="text" value={companyId} onChange={(e)=>setCompanyId(e.target.value)} required/>
-                        <label>Nama Bank</label>
-                        <Input className="form-custom" type="text" value={bankName} onChange={(e)=>setBankName(e.target.value)} required/>
-                        <label>Nombor Akaun Bank</label>
-                        <Input className="form-custom" type="text" value={bankAccount} onChange={(e)=>setBankAccount(e.target.value)} required/>
-                        <div className="tnc-section">
-                            <Label check>
-                                <Input type="checkbox" 
-                                    checked={setuju}
-                                    onChange={() => setSetuju(!setuju)}
-                                />
-                                <p>Dengan mendaftar untuk sebagai Vendor PlanKawen, saya mengaku sudah membaca serta bersetuju dengan <a href="/terma">Terma dan Syarat</a> yang telah ditetapkan</p>
-                            </Label>
-                        </div>
-                    </div>
-                    <div>
-                        <label>SSM</label>
-                        <div className="file-upload">
-                            <label htmlFor="upload" className="file-upload__label">Upload file here</label>
-                            <Filebase64 className="file-upload__input" id="testingUpload" multiple={ false } onDone={(x) => uploadOnDone(x) } />
-                        </div>
-                        <label>No. Telefon</label>
-                        <Input className="form-custom" type="text" value={phoneNo} onChange={(e)=>setPhoneNo(e.target.value)} required/>
-                        <label>Instagram</label>
-                        <Input className="form-custom" type="text" value={instagram} onChange={(e)=>setInstagram(e.target.value)} required/>
-                        <label>Facebook</label>
-                        <Input className="form-custom" type="text" value={facebook} onChange={(e)=>setFacebook(e.target.value)} required/>
-                        <div className="">
-                            <label>Kawasan</label>
-                            <div className="kawasan-section">
-                                {gMapsCities.map( (cty, index) =>{
-                                    let bandar = cty.state;
-                                    let chckd  = false;
-                                    if (cityArray != null) {
-                                        chckd = cityArray.includes(bandar) ? true : false;
-                                    }
-                                    return(
-                                        <div key={index} className="area-covered-div">
-                                            <label>
-                                                <input type="checkbox"
-                                                    name={bandar} 
-                                                    checked={chckd}
-                                                    onChange={(e) => handleChangeKawasan(e)}
-                                                />
-                                                {bandar}
-                                            </label>
-                                        </div>
-                                    )
-                                } )}
+
+                    {
+                        firstDiv == true ?
+                        <React.Fragment>
+                            <div>
+                                <label>Email Syarikat</label>
+                                <Input className="form-custom" type="email" value={companyEmail} onChange={(e)=>setCompanyEmail(e.target.value)} required/>
+                                <label>Password</label>
+                                <Input className="form-custom" type="password" value={password} onChange={(e)=>setPassword(e.target.value)} required/>
+                                <div className="position-button">
+                                    <Button color="primary" className="btn-daftar" onClick={()=>{
+                                        setFirstDiv(false)
+                                        setSecondDiv(true)
+                                    }}>Next1</Button>
+                                </div>
+                            </div>    
+
+                        </React.Fragment>
+                        : ''
+                    }
+                    {
+                        secondDiv == true ?
+                        <React.Fragment>
+                            <div>
+                                <label>Nama Syarikat</label>
+                                <Input className="form-custom" type="text" value={companyName} onChange={(e)=>setCompanyName(e.target.value)} required/>
+                                <label>Alamat Syarikat</label>
+                                <Input className="form-custom" type="textarea" value={companyAddress} onChange={(e)=>setCompanyAddress(e.target.value)} required/>
+                                <label>Nama Pemilik</label>
+                                <Input className="form-custom" type="text" value={owner} onChange={(e)=>setOwner(e.target.value)} required/>
+                                <label>Nombor Pendaftaran Syarikat</label>
+                                <Input className="form-custom" type="text" value={companyId} onChange={(e)=>setCompanyId(e.target.value)} required/>
+                                <label>SSM</label>
+                                <div className="file-upload">
+                                    <label htmlFor="upload" className="file-upload__label">Upload file here</label>
+                                    <Filebase64 className="file-upload__input" id="testingUpload" multiple={ false } onDone={(x) => uploadOnDone(x) } />
+                                </div>
+                                <div className="position-button">
+                                    <Button color="primary" className="btn-daftar" onClick={()=>{
+                                        setSecondDiv(false)
+                                        setFirstDiv(true)
+                                    }}>Back</Button>
+                                    <Button color="primary" className="btn-daftar" onClick={()=>{
+                                        setSecondDiv(false)
+                                        setThirdDiv(true)
+                                    }}>Next1</Button>
+                                </div>
                             </div>
-                        </div>
-                        <div className="position-button">
-                            <Button color="primary" className="btn-daftar" onClick={()=>submitForm()}>Daftar Masuk</Button>
-                        </div>
-                    </div>
+                        </React.Fragment>
+                        : ''
+                    }
+                    {
+                        thirdDiv == true ?
+                            <React.Fragment>
+                                <div>
+                                    <label>Nama Bank</label>
+                                    <Input className="form-custom" type="text" value={bankName} onChange={(e)=>setBankName(e.target.value)} required/>
+                                    <label>Nombor Akaun Bank</label>
+                                    <Input className="form-custom" type="text" value={bankAccount} onChange={(e)=>setBankAccount(e.target.value)} required/>
+                                    <label>No. Telefon</label>
+                                    <Input className="form-custom" type="text" value={phoneNo} onChange={(e)=>setPhoneNo(e.target.value)} required/>
+                                    <div className="position-button">
+                                        <Button color="primary" className="btn-daftar" onClick={()=>{
+                                            setSecondDiv(true)
+                                            setThirdDiv(false)
+                                        }}>Prev1</Button>
+                                         <Button color="primary" className="btn-daftar" onClick={()=>{
+                                            setThirdDiv(false)
+                                            setForthDiv(true)
+                                        }}>Next1</Button>
+                                    </div>
+                                </div>
+                            </React.Fragment>
+                            : ''
+                    }
+
+                    {
+                        forthDiv == true ?
+                        <React.Fragment>
+                            <div>   
+                                <label>Instagram</label>
+                                <Input className="form-custom" type="text" value={instagram} onChange={(e)=>setInstagram(e.target.value)} required/>
+                                <label>Facebook</label>
+                                <Input className="form-custom" type="text" value={facebook} onChange={(e)=>setFacebook(e.target.value)} required/>
+                                <div className="">
+                                    <label>Kawasan</label>
+                                    <div className="kawasan-section">
+                                        {gMapsCities.map( (cty, index) =>{
+                                            let bandar = cty.state;
+                                            let chckd  = false;
+                                            if (cityArray != null) {
+                                                chckd = cityArray.includes(bandar) ? true : false;
+                                            }
+                                            return(
+                                                <div key={index} className="area-covered-div">
+                                                    <label>
+                                                        <input type="checkbox"
+                                                            name={bandar} 
+                                                            checked={chckd}
+                                                            onChange={(e) => handleChangeKawasan(e)}
+                                                        />
+                                                        {bandar}
+                                                    </label>
+                                                </div>
+                                            )
+                                        } )}
+                                    </div>
+                                </div>
+                                <div className="tnc-section">
+                                    <Label check>
+                                        <Input type="checkbox" 
+                                            checked={setuju}
+                                            onChange={() => setSetuju(!setuju)}
+                                        />
+                                        <p>Dengan mendaftar untuk sebagai Vendor PlanKawen, saya mengaku sudah membaca serta bersetuju dengan <a href="/terma">Terma dan Syarat</a> yang telah ditetapkan</p>
+                                    </Label>
+                                </div>
+                                <div className="position-button">
+                                    <Button color="primary" className="btn-daftar" onClick={()=>{
+                                            setThirdDiv(true)
+                                            setForthDiv(false)
+                                    }}>Prev1</Button>
+                                    <Button color="primary" className="btn-daftar" onClick={()=>submitForm()}>Daftar Masuk</Button>
+                                </div>
+                            </div>
+                        </React.Fragment>
+                        : ''
+                    } 
                 </div>
             </div>
             
             <style jsx>{`
                 a { margin-top: 10px;}
+                .master-layout { padding: 0;}
                 .bg-color { background: rgb(244,244,244); background: linear-gradient(180deg, rgba(244,244,244,1) 80%, rgba(245,154,134,0.5732668067226891) 100%); height: 100vh; width: 100%; position: relative; }
                 .logo-container { text-align: center; margin-bottom: 10px; }
                 .logo-container > img { width: 50%; }
                 .form-custom { border: 2px solid #EAEAEA; }
                 .vendor-form-section { background-color: #DFDFDF; padding: 20px; border-radius: 10px; }
                 .vendor-form-section > h3 { font-weight: bold;}
-                .vendor-form { display: flex;  justify-content: space-between;}
-                .vendor-form > div { width: 48%; position: relative;}
+                .vendor-form-section { display: flex; align-items: center; justify-content: center;}
+                .vendor-form-section > div { }
+                .vendor-form { width: 50%; }
+                .vendor-form > div { position: relative;}
                 label { font-weight: 400; color: #3e3e3e; font-size: 14px; margin: 10px 0px 0px 0; }
                 .testing::after { content:"another word" !important; }
                 .file-upload { position: relative; }
                 .file-upload__label { display: block; padding: 7px; color: #fff; background: #F5F6FA; color: #3e3e3e; border-radius: 4px; border: 2px solid #EAEAEA; transition: background .3s; cursor: pointer;  height: 38px; margin: 0;}
                 .file-upload__label:hover { cursor: pointer; background: #000; }
                 .file-upload__input { position: absolute; left: 0; top: 0; right: 0; bottom: 0; font-size: 1; width:0; height: 100%; opacity: 0; }
-                .position-button  { text-align: center; position: absolute; width: 100%; bottom: 0; }
+                .position-button  { text-align: center; margin-top: 30px;}
                 .kawasan-section { display: flex; flex-wrap: wrap;}
                 .area-covered-div { margin-right: 10px; }
                 .area-covered-div > label { font-weight: 400; color: #3E3E3E; font-size: 14px;}
                 .area-covered-div > label > input { margin-right: 5px; }
-                .btn-daftar { background-color: #4bd19f; }
+                .position-button > Button { background-color: #4bd19f; margin: 20px; }
                 .tnc-section { font-size: 10px; padding: 10px 0px 0px 20px;}
             `}</style>
         </div>
