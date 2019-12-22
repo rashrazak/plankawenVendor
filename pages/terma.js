b88c554629511aa753e07ce929a5047fe9f382b3
HaziqMohsin <myhaziq3@gmail.com>
2019-12-22 18:38:29 +0800

    [Update] Terma rentak muzik terkini

--- pages/terma.js
+++ pages/terma.js
@@ -15,18 +15,78 @@ function terma (){
                     <p style={{'textAlign':'justify'}}>Apabila anda melayari portal web Plankawen.com, anda dianggap telah memahami Terma Penggunaan yang ditetapkan. Sekiranya anda tidak bersetuju dengan Terma Penggunaan yang dinyatakan, anda tidak perlu menggunakan portal web ini</p>
                     
                     <h4><b>1.Pihak dalam Plankawen.com</b> </h4>
-                    <h4><b>2.Pihak dalam Plankawen.com</b> </h4>
-                    <h4><b>3.Pihak dalam Plankawen.com</b> </h4>
-                    <h4><b>4.Pihak dalam Plankawen.com</b> </h4>
-                    <h4><b>5.Pihak dalam Plankawen.com</b> </h4>
-                    <h4><b>6.Pihak dalam Plankawen.com</b> </h4>
-                    <h4><b>7.Pihak dalam Plankawen.com</b> </h4>
-                    <h4><b>8.Pihak dalam Plankawen.com</b> </h4>
-                    <h4><b>9.Pihak dalam Plankawen.com</b> </h4>
-                    <h4><b>10.Pihak dalam Plankawen.com</b> </h4>
-                    <h4><b>11.Pihak dalam Plankawen.com</b> </h4>
-                    <h4><b>12.Pihak dalam Plankawen.com</b> </h4>
-                    <h4><b>13.Pihak dalam Plankawen.com</b> </h4>
+                    <p style={{'textAlign':'justify'}}>User / Pengguna – ialah mereka yang setuju dengan terma dan syarat, mendaftarkan diri dalam Plankawen sebagai pihak yang  menggunakan platform Plankawen sebagai cara untuk mengaturcara majlis perkahwinan mereka dengan memilih servis / tawaran / barangan yang dijual oleh Pihak Vendor di platform Plankawen.</p>
+                    <p style={{'textAlign':'justify'}}>Vendor – ialah mereka yang setuju dengan terma dan syarat, mendaftarkan pihak mereka dalam Plankawen sebagai pihak yang menggunakan Platform Plankawen sebagai cara untuk mengiklankan serta mengatur servis mereka untuk memudahkan carian oleh Pengguna. </p>
+                    <p style={{'textAlign':'justify'}}>Plankawen – ialah pihak yang menyediakan platform Plankawen sebagai sebuah website untuk User dan Vendor yang memudahkan carian dan pengiklanan, serta pengurusan majlis dan servis berkaitan hal perkahwinan dan sebagainya.</p>
+
+                    <h4><b>2.Had Tanggungjawab</b> </h4>
+                    <p style={{'textAlign':'justify'}}>Dengan bersetuju kepada Terma dan Syarat kami, anda mengakui bahawa Plankawen hanyalah laman sesawang yang membantu setiap pihak untuk menguruskan perancangan perkahwinan dan memudahkan pengiklanan servis perkahwinan sahaja.</p>
+                    <p style={{'textAlign':'justify'}}>Anda dengan jelas memahami dan bersetuju bahawa Plankawen.com tidak akan bertanggungjawab ke atas sebarang bentuk kerugian, secara langsung atau tidak langsung, yang berkaitan atau khusus, turutan atau exemplary, juga tidak terhad kepada kerugian berbentuk kehilangan keuntungan, niat baik, kepercayaan atau kerugian ketara atau tidak ketara yang lain akibat daripada tetapi tidak terhad kepada:</p>
+                    <ul>
+                        <li>akses tanpa kebenaran atau perubahan dalam penghantaran atau data anda;</li>
+                        <li>kenyataan atau tindakan Pihak Ketiga di portal web ini;</li>
+                        <li>penggunaan atau ketidakupayaan dalam menggunakan perkhidmatan ini;</li>
+                        <li>kos perolehan barangan dan/atau perkhidmatan gantian berikutan pembelian sebarang barangan, data, maklumat atau perkhidmatan atau mesej yang diterima atau transaksi yang dibuat menerusi atau daripada portal web ini; atau</li>
+                        <li>perkara-perkara lain yang berkaitan dengan portal web ini.</li>
+                    </ul>
+
+                    <h4><b>3.Pautan</b> </h4>
+                    <p style={{'textAlign':'justify'}}>Plankawen.com akan menyediakan pautan-pautan kepada laman web yang lain. Laman-laman web tersebut dimiliki dan dikendalikan oleh Pihak Ketiga dan oleh itu, Plankawen.com tidak mempunyai sebarang kawalan ke atas laman dan sumber tersebut. Anda mengakui dan bersetuju bahawa Plankawen.com tidak bertanggungjawab terhadap laman dan sumber luar tersebut dan tidak menyokong serta tidak bertanggungjawab ke atas sebarang kandungan, iklan, produk atau bahan-bahan lain yang disediakan di laman dan sumber tersebut. Anda seterusnya mengakui dan bersetuju bahawa Plankawen.com tidak bertanggungjawab, secara langsung mahupun tidak langsung, terhadap sebarang kerosakan atau kerugian yang disebabkan oleh atau dipercayai sebagai sebabnya atau dikaitkan dengan penggunaan atau pergantungan ke atas kandungan, barangan atau perkhidmatan yang disediakan di laman atau sumber tersebut, atau menerusi maklumat, laman atau sumber tersebut.</p>
+
+                    <h4><b>4.Penamatan</b> </h4>
+                    <p style={{'textAlign':'justify'}}>Plankawen.com berhak menamatkan akses anda kepada mana-mana bahagian perkhidmatan atau seluruh perkhidmatan dan mana-mana perkhidmatan yang berkaitan pada bila-bila masa, dengan sebab atau tanpa sebab, dengan notis atau tanpa notis, dan berkuatkuasa dengan serta-merta. Plankawen.com juga berhak menamatkan atau menggantung akaun anda yang tidak aktif, iaitu, didefinisikan sebagai kegagalan untuk menggunakan perkhidmatan portal web menerusi akaun tersebut bagi suatu tempoh tertentu. Anda bersetuju bahawa Plankawen.com tidak akan bertanggungjawab kepada anda atau kepada mana-mana Pihak Ketiga disebabkan oleh penamatan akses kepada perkhidmatan tersebut.</p>
+                    <p style={{'textAlign':'justify'}}>Apabila perkhidmatan ditamatkan, anda hendaklah berhenti daripada memasuki portal web kami dan kami akan segera membatalkan segala kata laluan dan pengenalan akaun yang telah diberikan kepada anda dan menafikan kemasukan ke semua laman web ini atau mana-mana bahagian daripadanya.</p>
+
+
+                    <h4><b>5.Perubahan Kepada Syarat-Syarat Perkhidmatan</b> </h4>
+                    <p style={{'textAlign':'justify'}}>Plankawen.com mempunyai hak untuk membuat sebarang penambahan, perubahan, pengubahsuaian atau pembatalan kepada mana-mana syarat ini pada bila-bila masa dengan memberi notis terlebih dahulu mahupun tanpa memberi notis. Walau bagaimanapun, dalam keadaan kecemasan atau demi menjaga keselamatan portal web atau dalam keadaan di luar kawalan dan jangkaan, di mana Plankawen.com mendapati perlunya untuk menambah, mengubah, mengubahsuai atau membatal mana-mana syarat ini, kami berhak berbuat demikian tanpa memberi notis terlebih dahulu kepada anda. Adalah dipersetujui bahawa anda akan mengakses dan meneliti syarat-syarat ini dari semasa ke semasa untuk mengetahui sebarang penambahan, perubahan, pengubahsuaian atau pembatalan yang terkini. Anda selanjutnya bersetuju dan menerima bahawa akses secara berterusan kepada Plankawen.com dan penggunaan syarat-syarat (yang diubah atau diubahsuai dari semasa ke semasa) akan dianggap sebagai penerimaan anda terhadap sebarang penambahan, perubahan, pengubahsuaian atau pembatalan kepada syarat-syarat ini.</p>
+
+                    <h4><b>6.Pengubahsuaian Kepada Perkhidmatan</b> </h4>
+                    <p style={{'textAlign':'justify'}}>Plankawen.com mempunyai hak untuk mengubahsuai atau menamatkan perkhidmatan (atau sebahagian daripada perkhidmatan) sama ada secara sementara atau tetap pada bila-bila masa sahaja dengan notis atau tanpa notis. Anda bersetuju bahawa Plankawen.com tidak bertanggungjawab kepada anda atau mana-mana Pihak Ketiga atas sebarang pengubahsuaian, penggantungan atau penamatan perkhidmatan Plankawen.com.</p>
+
+
+                    <h4><b>7.Pembayaran Dalam Talian</b> </h4>
+                    <p style={{'textAlign':'justify'}}>Pihak Plankawen.com tidak akan menerima sebarang liabiliti bagi bayaran yang tidak diterima oleh akaun agensi yang betul jika anda memasukkan nombor akaun yang tidak betul atau butiran peribadi yang tidak tepat. Kami juga tidak akan menerima sebarang liabiliti jika bayaran anda tidak dibenarkan atau ditolak oleh pembekal kad kredit / debit anda bagi apa-apa sebab.</p>
+                    <p style={{'textAlign':'justify'}}>Jika pembekal kad kredit / debit menolak pembayaran anda, Plankawen.com tidak bertanggungjawab untuk membawa perkara ini kepada perhatian anda. Anda perlu menyemak dengan bank / pembekal kad kredit / pembekal kad debit anda jika ada pembayaran yang telah ditolak dari akaun anda.</p>
+                    <p style={{'textAlign':'justify'}}>Plankawen.com, dalam apa jua keadaan akan tidak bertanggungjawab ke atas apa-apa jua kerugian yang timbul daripada penggunaan, ketidakupayaan untuk menggunakan, atau akibat daripada penggunaan portal web ini, mana-mana laman web yang dikaitkan dengan portal web ini, atau bahan-bahan atau maklumat yang terdapat di mana-mana atau semua laman-laman tersebut, sama ada berdasarkan jaminan, kontrak, tort atau mana-mana teori undang-undang lain dan sama ada atau tidak dimaklumkan tentang kemungkinan kerosakan tersebut.</p>
+                    <p style={{'textAlign':'justify'}}>Jumlah pembayaran untuk langganan iklan di dalam Plankawen.com hendaklah dibuat secara penuh dan tidak boleh dibuat secara ansuran untuk setiap langganan pakej pengiklanan.</p>
+
+                    <h4><b>8.Pengesahan Pembayaran</b> </h4>
+                    <p style={{'textAlign':'justify'}}>Anda akan menerima pengesahan bayaran anda pada skrin anda selepas anda membuat pembayaran. Anda boleh mencetak bukti pembayaran apabila transaksi anda telah selesai (Berjaya) untuk rekod anda sendiri.</p>
+
+                    <h4><b>9.Dasar Bayaran Balik</b> </h4>
+                    <p style={{'textAlign':'justify'}}>Sekiranya anda mempunyai sebarang pertanyaan mengenai bayaran berlebihan terhadap akaun anda dan merasakan yang anda mungkin layak untuk bayaran balik, sila hubungi agensi yang berkenaan dengan menyatakan bukti pengesahan pembayaran atau nombor rujukan.</p>
+
+                    <h4><b>10.Dasar Pengembalian Wang (Refund)</b> </h4>
+                    <p style={{'textAlign':'justify'}}>Tiada pengembalian wang untuk sebarang pembayaran yang telah dibuat kepada pihak ketiga yang menyediakan servis/perkhidmatan/barang perkahwinan, dan juga langganan iklan di dalam Plankawen.com. Jika terdapat sebarang kesilapan atau masalah, sila hubungi kami melalui Whatsapp, PM Facebook Page dan kami akan cuba untuk menguruskannya secepat mungkin</p>
+
+
+                    <h4><b>11.Keselamatan</b> </h4>
+                    <p style={{'textAlign':'justify'}}>Semua butir-butir pembayaran yang dimasukkan melalui gerbang pembayaran di dalam portal web ini adalah disulitkan (encrypted) apabila anda, atau pengguna Pihak Ketiga membuat pembayaran. Plankawen.com tidak akan bertanggungjawab bagi apa-apa kegagalan oleh pengguna dalam talian atau Pihak Ketiga dalam membuat pembayaran untuk melindungi data pada skrin mereka daripada dilihat orang lain atau diperolehi oleh mereka yang terbabit secara sengaja mahupun tidak sengaja, semasa proses pembayaran dalam talian atau berkenaan dengan apa-apa peninggalan maklumat dalam masa proses pembayaran dalam talian.</p>
+
+                    <h4><b>12.Undang-Undang & Kehakiman Yang Dipakai</b> </h4>
+                    <p style={{'textAlign':'justify'}}>Terma Penggunaan ini hendaklah diterjemahkan dan tertakluk kepada undang-undang yang dikuatkuasakan di Malaysia. Bergantung kepada pihak pengadilan, setiap pihak dengan ini bersetuju untuk menyerahkan kehakiman kepada mahkamah Kerajaan Malaysia untuk sebarang bantahan.</p>
+
+                    <h4><b>13.Umum</b> </h4>
+                    <p style={{'textAlign':'justify'}}>Sekiranya mana-mana peruntukan syarat-syarat ini menyalahi undang-undang atau tidak sah di sisi undang-undang atau peraturan-peraturan masa kini atau yang mendatang, peruntukan tersebut akan diasingkan dan syarat-syarat ini akan ditafsirkan seolah-olah peruntukan yang tidak sah itu tidak wujud dan syarat-syarat ini bersama-sama peruntukan lain akan berkuatkuasa sepenuhnya tanpa dijejaskan oleh peruntukan yang menyalahi undang-undang atau yang tidak sah.</p>
+
+                    <h5><b>Informasi Diperlukan sebagai User:</b></h5>
+                    <p>Nama Pengguna :</p>
+                    <p>Nama Pasangan : optional</p>
+                    <p>Tarikh Majlis :</p>
+                    <p>Lokasi Majlis :</p>
+
+                    <h5><b>Informasi diperlukan sebagai vendor</b></h5>
+                    <p>Nama Syarikat :</p>
+                    <p>Nama Pemilik : (boleh tambah jika partnership)</p>
+                    <p>Lokasi Syarikat :</p>
+                    <p>No. Pendaftaran Syarikat :</p>
+                    <p>Alamat Syarikat :</p>
+                    <p>No. Tel Syarikat :</p>
+                    <p>Akaun Syarikat :</p>
+                    <p>Nombor Akaun syarikat :</p>
+
+
                     </div> 
                     {/* <button onClick={(e)=>submitLoading(e)}></button> */}
                     <style jsx>{`
