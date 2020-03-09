c28bfab93a2f95d753c5fe262c9b9c82799b1535
HaziqMohsin <myhaziq3@gmail.com>
2020-03-09 13:21:36 +0800

    [Update] uploadservice fix image

--- components/dashboard/uploadService.js
+++ components/dashboard/uploadService.js
@@ -122,8 +122,8 @@ function uploadService() {
                 .upload-service p { margin: 0; font-size: 12px; color: #3E3E3E;}
                 .upload-service p > span { margin-right: 5px;}
                 .card-flex { display: flex; flex-wrap: wrap;}
-                .card-service { border-radius: 5px; width: 150px; box-shadow: 0 0 4px 0 rgba(0,0,0,0.2); margin-right: 10px; margin-bottom: 20px;}
-                .card-service > img { object-fit: cover; width: 100%;} 
+                .card-service { border-radius: 5px; width: 150px; box-shadow: 0 0 4px 0 rgba(0,0,0,0.2); margin-right: 10px; margin-bottom: 20px; background-color: #FFF;}
+                .card-service > img { object-fit: cover; width: 100%; height: 85px; object-position: center;} 
                 .card-service p { background-color: #FFF; margin: 0; padding: 10px; border-radius: 0px 0px 5px 5px;}
                 .card-service p span img { margin-right: 10px;}
                 .card-service-add { width: 100px; background-color: #EBF9F8;}
