b84170c824cc19889cddce3d0e5331127e7a16e6
HaziqMohsin <myhaziq3@gmail.com>
2020-03-23 17:47:09 +0800

    [Update] Hover image on user image profile

--- components/dashboard/profileDetails.js
+++ components/dashboard/profileDetails.js
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useState} from 'react'
+import React, {useContext, useEffect, useState, useRef} from 'react'
 import LoginContext from '../../contexts/LoginContext'
 import firebase from '../../config/firebaseConfig'
 
@@ -7,8 +7,35 @@ function profileDetails() {
     const {user,signOut,saveVendorDetails} = useContext(LoginContext);
     const [data, setdata] = useState(false)
     const [editDetails, setEditDetails] = useState(false)
+    const [hoverImage, setHoverImage] = useHover();
 
     const toggle = () => setEditDetails(!editDetails)
+
+    function useHover () {
+
+        const [value, setValue] = useState(false)
+        const ref = useRef(null)
+
+        const handleMouseOver = () => setValue(true)
+        const handleMouseOut = () => setValue(false)
+
+        useEffect(() => {
+            
+            const node = ref.current;
+            if (node) {
+                node.addEventListener('mouseover', handleMouseOver);
+                node.addEventListener('mouseout', handleMouseOut);
+
+                return () => {
+                    node.addEventListener('mouseover', handleMouseOver);
+                    node.addEventListener('mouseout', handleMouseOut);
+                }   
+            }
+           
+        }, [ref.current])
+
+        return [ref, value];
+    }
 
 
     useEffect( () => {
@@ -32,12 +59,16 @@ function profileDetails() {
         <div>
             <div className={`profile-details`}>
                 <div className={`oval-kampret`}>
-                    <div className={`oval-pic`}>
+                    <div className={`oval-pic`} ref={hoverImage}>
                         {
-                            data == true ?
-                            <img src={user.photoUrl}/>
-                            :
-                            <img src="/images/icon/user-placeholder.png"/>
+                            setHoverImage ?
+                                <img className={`user-hover`} src="/images/icon/user-hover.png"/>
+                            : 
+                                data == true ?
+                                <img src={user.photoUrl}/>
+                                :
+                                <img className={`user-placholder`} src="/images/icon/user-placeholder.png"/>
+                           
                         }
                     </div>
                     <p>Disahkan SSM</p>
@@ -61,17 +92,21 @@ function profileDetails() {
                 .profile-details { background-color: #F5F6FA;  padding: 10px 20px; position: relative; margin: 20px 0; min-height: 107px; border-radius: 4px;}
                 .oval-kampret { position: absolute; top: -45px; left: 34px;}
                 .oval-kampret > p { text-align: center; font-style: italic; color: #47CBC4; font-size: 12px; margin-top: 10px;}
-                .oval-pic { width: 112px; height: 112px; border-radius: 50%; background-color: #9B9B9B; overflow: hidden;}
+                .oval-pic { width: 112px; height: 112px; border-radius: 50%; background-color: #9B9B9B; overflow: hidden; position: relative; cursor: pointer;}
+                .oval-pic:hover { box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.2); background-color: #7C7C7C;}
                 .oval-pic img { object-fit: cover; width: 100%;}
+                .oval-pic img.user-placholder {object-fit: unset; position: absolute; top: 50%; left: 50%; transform: translate(-50%,-50%); width: 40px;}
+                .oval-pic img.user-hover {object-fit: unset; position: absolute; top: 50%; left: 50%; transform: translate(-50%,-50%); width: 40px;}
                 .profile-upper { margin-left: 156px;}
                 h2 { margin: 0; font-size: 22px; color: #3e3e3e; font-weight: normal;}
                 h2 > span { font-weight: bold;}
                 p { margin: 0; font-size: 12px; color: #3e3e3e; font-weight: normal;}
-                .form-textarea { background-color: #FFF;border: 1px solid #EAEAEA;border-radius: 4px; resize: none; height: 80px; width: 100%; margin-top: 10px; padding: 10px 20px; padding-right: 70px; font-weight: normal; font-size: 14px;}
+                .form-textarea { background-color: #FFF;border: 1px solid #EAEAEA;border-radius: 4px; resize: none; height: 80px; width: 100%; margin-top: 10px; padding: 10px 20px; padding-right: 70px; font-weight: normal; font-size: 14px; color: #2B2B2B;}
                 .form-textarea.active { box-shadow: 0px 6px 15px rgba(0, 0, 0, 0.2); transition: all .3s;}
                 .keterangan-div { position: relative;}
                 .icon-edit { position: absolute; right: 10px; top: 20px; width: 24px; cursor: pointer;}
                 .p-save { color: #47CBC4; font-weight: bold; font-size: 14px; width: auto;}
+                
             `}</style>
         </div>
     )
