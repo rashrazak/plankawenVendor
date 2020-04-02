d4834a260e556ab44978da595d74ab5bc80fb6b3
HaziqMohsin <myhaziq3@gmail.com>
2020-04-02 20:38:13 +0800

    [Update] new icon for service type and fix dashboard

--- components/dashboard/uploadService.js
+++ components/dashboard/uploadService.js
@@ -110,7 +110,10 @@ function uploadService() {
                                 return(
                                     <div key={i} className={`card-service`} onClick={()=>editFunction(i)}>
                                         <img src={img}/>
-                                        <p><span><img src="/images/icon/ico-venue-black.png"/> {v.serviceType} - {v.serviceName}</span></p>
+                                        <div className={`card-service-desc`}>
+                                            <img className={`icon-service`} src="/images/icon/services-icon/dark/ico-venue.png"/>
+                                            <p>{v.serviceType} - {v.serviceName}</p>
+                                        </div>
                                     </div>
                                 )
                             })
@@ -142,10 +145,11 @@ function uploadService() {
                 .upload-service p { margin: 0; font-size: 12px; color: #3E3E3E;}
                 .upload-service p > span { margin-right: 5px;}
                 .card-flex { display: flex; flex-wrap: wrap;}
-                .card-service { border-radius: 5px; width: 150px; box-shadow: 0 0 4px 0 rgba(0,0,0,0.2); margin-right: 10px; margin-bottom: 20px; background-color: #FFF;}
-                .card-service > img { object-fit: cover; width: 100%; height: 85px; object-position: center;} 
-                .card-service p { background-color: #FFF; margin: 0; padding: 10px; border-radius: 0px 0px 5px 5px;}
-                .card-service p span img { margin-right: 10px;}
+                .card-service { border-radius: 5px; width: 150px; box-shadow: 0 0 4px 0 rgba(0,0,0,0.2); margin-right: 10px; margin-bottom: 20px; background-color: #FFF;}                
+                .card-service > img { object-fit: cover; width: 100%; height: 85px; object-position: center;}
+                .card-service-desc {  background-color: #FFF; margin: 0; padding: 10px; border-radius: 0px 0px 5px 5px; display: flex; align-items: flex-start;}
+                .card-service-desc > p {  font-style: normal; font-weight: 900;font-size: 0.75rem; color: #3E3E3E; margin: 0;}
+                .card-service-desc > img {width: 15px; flex: 0 0 15px; margin-right: 15px;}            
                 .card-service-add { width: 100px; background-color: #EBF9F8; cursor: pointer; height: 100px;}
                 .card-service-add p { background-color: transparent;}
                 .card-service-add { background-image: url(/images/icon/plus-circle-dark.png); background-repeat: no-repeat; background-position: center 50%;}
